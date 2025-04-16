72183a8a578565ff29583419078baf60d320d447
Mohammed <125723093+mohammad20101368@users.noreply.github.com>
2025-04-16 03:02:48 -0700

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "5b134b89-f55a-4a02-9ef0-2d91a7f8eca3";
 
 const proxyIPs = ["ts.hpc.tw"]; 
 const cn_hostnames = [''];
